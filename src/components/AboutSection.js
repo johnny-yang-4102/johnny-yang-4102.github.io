eab990a57e1595a50ae8f3afbf73d8b0d1a262e8
Johnny <jyang8501@gmail.com>
2022-04-02 10:50:17 -0400

    Complete initial website sections

---
+++ src/components/AboutSection.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AboutSection = ({ }) => {
+  return (
+    <div>
+      <p>about us</p>
+    </div>
+  )
+}
+
+export default AboutSection
---
+++ src/components/ProjectSection.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Project from './Project'
+
+const ProjectSection = ({ }) => {
+  return (
+    <div>
+      <p>projectSection</p>
+    </div>
+  )
+}
+
+export default ProjectSection
