e17481eed63196d7b9e9927b94f98807f39cd5a0
Johnny <johnny@M1MBP.local>
2022-07-18 20:00:38 -0400

    Fix divider issue

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import NavBar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
 import ContactSection from "./components/ContactSection";
-import { Stack, Divider, Typography } from "@mui/material";
+import { Stack, Divider, Typography, Grid } from "@mui/material";
 
 
 function App() {
   return (
     <Stack spacing={4} direction="column">
-      <NavBar/>
-      <Divider textAlign="center" variant="inset"><Typography variant="h3" id="about">About</Typography></Divider>
-      <AboutSection/>
-      <Divider textAlign="center" ><Typography variant="h3" id="projects">Projects</Typography></Divider>
-      <ProjectSection/>
-      <Divider textAlign="center"><Typography variant="h3" id="contact">Contact</Typography></Divider>
-      <ContactSection/>
+      <NavBar />
+
+      <Grid item>
+        <Divider textAlign="center" variant="inset"><Typography variant="h3" id="about">About</Typography></Divider>
+      </Grid>
+
+      <AboutSection />
+
+      <Grid item>
+        <Divider textAlign="center" ><Typography variant="h3" id="projects">Projects</Typography></Divider>
+      </Grid>
+      <ProjectSection />
+
+      <Grid item>
+        <Divider textAlign="center"><Typography variant="h3" id="contact">Contact</Typography></Divider>
+      </Grid>
+      
+      <ContactSection />
     </Stack>
   );
 }
